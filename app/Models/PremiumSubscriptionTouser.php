9737cafc13f998adfc0183db1b28441f6b8d8664
19moynul <19moynul@gmail.com>
2022-10-25 08:46:23 +0600

    added guarded in PremiumSubscriptionToUser

--- app/Models/PremiumSubscriptionTouser.php
+++ app/Models/PremiumSubscriptionTouser.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class PremiumSubscriptionToUser extends Model
 {
     use HasFactory;
-    public $table = 'tbl_premium_subscription_to_user';
+    protected $table  = 'tbl_premium_subscription_to_user';
+    public $guarded = [];
 }
