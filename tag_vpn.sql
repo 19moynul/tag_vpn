4beed6139283b8008ee0108df250fb158bace20e
19moynul <19moynul@gmail.com>
2022-10-31 08:13:00 +0600

    db updated

--- tag_vpn.sql
+++ tag_vpn.sql
@@ -32,7 +32,7 @@ CREATE TABLE `tbl_admin` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +41,7 @@ CREATE TABLE `tbl_admin` (
 
 LOCK TABLES `tbl_admin` WRITE;
 /*!40000 ALTER TABLE `tbl_admin` DISABLE KEYS */;
+INSERT INTO `tbl_admin` VALUES (1,'moynul','19moynul@gmail.com','$2y$10$cbRNyL9yUslfWlSkTflQkOQhgc7uz/zTawmDXYkVQ6u96IeZMGTGO',1,'2022-10-24 02:57:17','2022-10-25 20:43:21');
 /*!40000 ALTER TABLE `tbl_admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,7 +59,7 @@ CREATE TABLE `tbl_password_resets` (
   `user_type` int NOT NULL DEFAULT '1' COMMENT '1=admin 2=user',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -90,7 +91,7 @@ CREATE TABLE `tbl_premium_subscription` (
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,7 +100,7 @@ CREATE TABLE `tbl_premium_subscription` (
 
 LOCK TABLES `tbl_premium_subscription` WRITE;
 /*!40000 ALTER TABLE `tbl_premium_subscription` DISABLE KEYS */;
-INSERT INTO `tbl_premium_subscription` VALUES (1,'Anual','subtitle','1 month','1 day',1,100,'2022-10-23 03:17:48',NULL,NULL);
+INSERT INTO `tbl_premium_subscription` VALUES (1,'Anual','anual subscription','1 month','1 day',1,100,'2022-10-23 21:44:41','2022-10-24 03:48:06','2022-10-23 21:45:13'),(4,'Anual 2','anual subscription','1 month','1 day',1,200,'2022-10-23 21:47:36','2022-10-23 21:47:36',NULL);
 /*!40000 ALTER TABLE `tbl_premium_subscription` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -121,7 +122,7 @@ CREATE TABLE `tbl_premium_subscription_to_user` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -130,7 +131,35 @@ CREATE TABLE `tbl_premium_subscription_to_user` (
 
 LOCK TABLES `tbl_premium_subscription_to_user` WRITE;
 /*!40000 ALTER TABLE `tbl_premium_subscription_to_user` DISABLE KEYS */;
+INSERT INTO `tbl_premium_subscription_to_user` VALUES (1,1,1,'2022-10-24 02:02:18','2022-11-24 02:02:18','1 day',2,'2022-10-23 20:02:18','2022-10-23 22:42:37'),(2,1,4,'2022-10-26 05:10:10','2022-11-26 05:10:10','1 day',2,'2022-10-25 23:10:10','2022-10-25 23:10:42');
 /*!40000 ALTER TABLE `tbl_premium_subscription_to_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_server`
+--
+
+DROP TABLE IF EXISTS `tbl_server`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_server` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `ip` varchar(255) DEFAULT NULL,
+  `port` varchar(45) DEFAULT NULL,
+  `icon` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_server`
+--
+
+LOCK TABLES `tbl_server` WRITE;
+/*!40000 ALTER TABLE `tbl_server` DISABLE KEYS */;
+INSERT INTO `tbl_server` VALUES (1,'Bangladesh','192.168.0.1','100',NULL),(2,'Pakistan','192.168.0.1','200',NULL),(3,'India','192.168.0.1','300',NULL);
+/*!40000 ALTER TABLE `tbl_server` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -152,7 +181,7 @@ CREATE TABLE `tbl_user` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -161,8 +190,35 @@ CREATE TABLE `tbl_user` (
 
 LOCK TABLES `tbl_user` WRITE;
 /*!40000 ALTER TABLE `tbl_user` DISABLE KEYS */;
-INSERT INTO `tbl_user` VALUES (1,'moynul','19moynul@gmail.com','$2y$10$Sv6eOIFOYKq7n.wAYp/9HeSVGqmaEABe4YZOajzyHnUYWrrTKA4xa','normal',NULL,1,'2022-10-23 01:35:58','2022-10-22 20:55:37'),(2,'moynul','19moynuls@gmail.com','$2y$10$ZEXYOiK7JHL8NqooGXZer.9Zf92ecZHxzjsuI4TBxsQKuhcgBH.8S','normal',NULL,1,'2022-10-23 02:21:38',NULL),(3,'moynul','19smoynul@gmail.com','$2y$10$6687N61/3QUfLTM3zudHLuCaDGb9dQapB6kmbuDvC0KlnCo4otA0O','normal',NULL,1,'2022-10-23 04:51:56',NULL);
+INSERT INTO `tbl_user` VALUES (1,'moynul','19moynul@gmail.com','$2y$10$PBCiw0RW/vOYITDbuW./u.X1o9r7Fv4iZEPIgE6yuuuf54hLg2jli','normal',NULL,1,'2022-10-23 01:35:58','2022-10-25 20:20:24'),(2,'moynul','19moynuls@gmail.com','$2y$10$ZEXYOiK7JHL8NqooGXZer.9Zf92ecZHxzjsuI4TBxsQKuhcgBH.8S','normal',NULL,1,'2022-10-23 02:21:38',NULL),(3,'moynul','19smoynul@gmail.com','$2y$10$6687N61/3QUfLTM3zudHLuCaDGb9dQapB6kmbuDvC0KlnCo4otA0O','normal',NULL,1,'2022-10-23 04:51:56',NULL);
 /*!40000 ALTER TABLE `tbl_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_user_to_device`
+--
+
+DROP TABLE IF EXISTS `tbl_user_to_device`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_user_to_device` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `device_name` varchar(150) DEFAULT NULL,
+  `uid` varchar(150) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_user_to_device`
+--
+
+LOCK TABLES `tbl_user_to_device` WRITE;
+/*!40000 ALTER TABLE `tbl_user_to_device` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_user_to_device` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -181,7 +237,7 @@ CREATE TABLE `tbl_user_transaction_history` (
   `comment` text,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -202,4 +258,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-23 10:58:15
+-- Dump completed on 2022-10-31  8:04:14
